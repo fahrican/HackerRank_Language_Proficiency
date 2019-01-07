36b69dd5221a5096e47fdebdbcd66533c73c5b41
Fahri Can <fahri-can@gmx.at>
2019-01-07 13:23:11 +0100

    Update string_validators.py

--- python/string_validators.py
+++ python/string_validators.py
@@ -4,26 +4,11 @@ String Validators
 
 
 def string_validator(given_string: str):
-    if any(char.isalnum() for char in given_string):
-        print('True')
-    else:
-        print('False')
-    if any(char.isalpha() for char in given_string):
-        print('True')
-    else:
-        print('False')
-    if any(char.isdigit() for char in given_string):
-        print('True')
-    else:
-        print('False')
-    if any(char.islower() and char.isalpha() for char in given_string):
-        print('True')
-    else:
-        print('False')
-    if any(not char.isdigit() and char.isalpha() and char.isupper() for char in given_string):
-        print('True')
-    else:
-        print('False')
+    print(any(char.isalnum() for char in given_string))
+    print(any(char.isalpha() for char in given_string))
+    print(any(char.isdigit() for char in given_string))
+    print(any(not char.isdigit() and char.isalpha() and char.islower() for char in given_string))
+    print(any(not char.isdigit() and char.isalpha() and char.isupper() for char in given_string))
 
 
 if __name__ == '__main__':
